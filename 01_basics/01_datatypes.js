54af14dff1615643622dc82e969922afb86b5d50
Ayush Kumar Pandey <ayushpandey1974@gmail.com>
2026-04-20 21:35:58 +0000

    Added JavaScript basics: variables and datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,16 @@
+let name = "ayush"
+let age = 18
+let isLoggedIn = false
+
+// Data types //
+
+//numbers = 2 to power 53
+//bigint
+//string = ""
+//boolean
+//object
+//null = standalone value 
+// undefined
+//symbol = unique
+
+console.log(typeof "ayush");
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 14455
+let accountEmail = "ayush@gmail.com"
+var accountPassword = "2322"
+accountCity = "Jaipur"
+let accountState ;
+
+// accountId = 2 - Not allowed //
+
+accountEmail = "ayushkr@gmail.com"
+accountPassword = "3456"
+accountCity = "Bengaluru"
+
+console.table([accountEmail,accountId,accountPassword,accountState,accountCity]);
+
+
+console.log(accountId);
